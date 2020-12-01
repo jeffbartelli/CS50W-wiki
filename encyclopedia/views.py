e15eadd2e994f57e1e1f8cc27e5d8e461a3c2db5
jeffbartelli <jeff.bartelli@zoho.com>
2020-11-30 17:10:01 -0800

    updated search and edit funcs

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,21 +1,16 @@
+import random as rando
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.urls import reverse
 from django import forms
-
 from . import util
 from markdown2 import Markdown
-from random import choice
 
 class EntryForm(forms.Form):
-    title = forms.CharField(label="Title", widget=forms.TextInput(attrs={'class':'form-control col-md-8 col-lg-8'}))
-    content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control col-md-8 col-lg-8','rows':4,'cols':15}))
+    title = forms.CharField(label="Title", widget=forms.TextInput(attrs={'class':'form-control col-md-8'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control col-md-8','rows':4,'cols':15}))
     edit = forms.BooleanField(initial=False, widget=forms.HiddenInput(), required=False)
-
-def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
 
 def entry(request, entry):
     mark = Markdown()
@@ -30,28 +25,36 @@ def entry(request, entry):
             "entryTitle": entry    
         })
 
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
 def search(request):
     term = request.GET.get('q','')
-    if util.get_entry(term) is None:
+    if util.get_entry(term) != None:
+        return HttpResponseRedirect(reverse("entry", kwargs={'entry': term}))
+    else:
         partialTerms = []
         for n in util.list_entries():
-            if value.upper() in n.upper():
-                partialTerms.append(entry)
-        return render(request, "encyclopedia/index.html", {
-            "entries": partialTerms,
-            "search": True,
-            "value": value
-        })
-    else:
-        return HttpResponseRedirect(reverse("entry", kwargs={"entry": value}))
+            if term.upper() in n.upper():
+                partialTerms.append(n)
+        if len(partialTerms) == 0:
+            return render(request, "encyclopedia/noEntry.html")
+        else:
+            return render(request, "encyclopedia/index.html", {
+                "entries": partialTerms,
+                "search": True,
+                "value": term
+            })
 
 def newEntry(request):
     if request.method == "POST":
         entryForm = EntryForm(request.POST)
         if entryForm.is_valid():
-            title = entryForm.cleaned_data["title"]
+            title = entryForm.cleaned_data["title"].capitalize()
             content = entryForm.cleaned_data["content"]
-            if util.get_entry(title) is None or form.cleaned_data["edit"] is True:
+            if util.get_entry(title) is None or entryForm.cleaned_data["edit"] is True:
                 util.save_entry(title, content)
                 return HttpResponseRedirect(reverse("entry", kwargs={
                     'entry': title
@@ -92,5 +95,5 @@ def edit(request, entry):
         })
 
 def random(request):
-    randomize = random.choice(util.list_entries())
+    randomize = rando.choice(util.list_entries())
     return HttpResponseRedirect(reverse("entry", kwargs={"entry": randomize}))
